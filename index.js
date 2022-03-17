1e29b9f4c774e6f97741da64b5cf7553fbd040c7
= <=>
2022-03-17 18:34:06 +0100

    Responsive and filter

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-let search = document.querySelector("#search");
-let images = document.querySelectorAll(".card");
-console.log(images);
+function myFunction(){
+    let search = document.querySelector("#search").value;
+    search= search.toLowerCase();
+    let result = document.querySelectorAll(".card");
 
-search.oninput = ()=> {
-
-    images.forEach(hide => hide.style.display = "none");
-
-    let value = search.value;
-    images.forEach(filter =>{
-    let title = filter.getAttribute('data-title');
-if(value == title){ 
-    filter.style.display = 'block';
+    for (i = 0; i < result.length; i++) { 
+        if (!result[i].innerHTML.toLowerCase().includes(search)) {
+            result[i].style.display="none";
+        }else {
+            result[i].style.display="block";                 
+        }
+    }
 }
-})
-}
+
